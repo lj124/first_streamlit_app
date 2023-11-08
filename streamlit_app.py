0be013bd4b289fab94660c0dc37095bb6251b96a
lj124 <72022332+lj124@users.noreply.github.com>
2023-11-08 17:06:15 +0530

    Update streamlit_app.py
    Updated Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("New Healthy Diner!")
+streamlit.header("Breakfast Menu")
+streamlit.text("Oatmeal")
+streamlit.text("Strawberry Smoothie")
+streamlit.text("Boiled Eggs")

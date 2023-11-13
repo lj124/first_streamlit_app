04644b603a4137b3ec17427ded499d3a6d57c6e4
lj124 <72022332+lj124@users.noreply.github.com>
2023-11-13 13:17:33 +0530

    Update streamlit_app.py
    Data in json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,14 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list['Fruit']))
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header('Fruityvice Fruit Advice')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 

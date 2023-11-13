b592594cf0f7a5c66c86a34cd043660d1b915241
lj124 <72022332+lj124@users.noreply.github.com>
2023-11-13 13:30:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 #taking input
 fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # streamlit.text(fruityvice_response.json())
 # Normalize semi-structured JSON data into a flat table
